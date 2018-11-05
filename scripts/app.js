6e762f495c0444cd97cbaa94fbe6d6543b170927
DamienSmagghe <damien.smagghe@hetic.net>
2018-11-05 17:44:23 +0100

    :bug: Fix target overflow

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
-console.log('cc')
+const shooter = {}
+
+// SETUP
+
+shooter.$container = document.querySelector('.shooter')
+shooter.$start = shooter.$container.querySelector('.starter')
+shooter.$score = shooter.$container.querySelector('.score .value')
+shooter.$timer = shooter.$container.querySelector('.timer')
+shooter.$targets = shooter.$container.querySelector('.targets')
+
+// METHODS
+shooter.addTarget = () =>
+{
+    const $target = document.createElement('div')
+    $target.classList.add('target')
+    $target.style.top = `${Math.random() * 100}%`
+    $target.style.left = `${Math.random() * 100}%`
+    shooter.$targets.appendChild($target)
+}
+
+shooter.addTarget()
